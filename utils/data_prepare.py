510e9a7dd76b7673f694856c4e96250e4afc2d11
Taosheng-ty <taoyang@cs.utah.edu>
2019-12-06 11:37:53 -0700

    add binary method to binarize data

--- utils/data_prepare.py
+++ utils/data_prepare.py
@@ -1,6 +1,9 @@
 from scipy.sparse import csr_matrix
 import numpy as np
 from argparse import Namespace
+import sys
+sys.path.append("..")
+# from algorithms import return_label
 def read_libsvm(fname, num_features=0):
 	'''
 		Reads a libsvm formatted data and outputs the training set (sparse matrix)[1], 
@@ -33,17 +36,27 @@ def read_libsvm(fname, num_features=0):
 	X = csr_matrix((data, (row_ind, col_ind)), shape=(len(y), num_features))
 
 	return X, np.array(y), num_features
+def binary_data(data):
+    pos_ind=np.where(data>0)
+    data=data*0
+    data[pos_ind]=1
+    return data
 def get_data(arg):
     X_val, label_val, num_features=read_libsvm(arg.val_data)
     X_train, label_train, num_features=read_libsvm(arg.train_data)
     X_train=X_train.toarray()
     X_val=X_val.toarray()
-    X_train=(X_train-np.mean(X_train,0))/(np.std(X_train,0)+1e-5)
-    X_val=(X_val-np.mean(X_val,0))/(np.std(X_val,0)+1e-5)
     ind=np.where(label_val==0)
     label_val[ind]=-1
     ind=np.where(label_train==0)
     label_train[ind]=-1
+    if arg.norm==True:
+        
+        X_train=(X_train-np.mean(X_train,0))/(np.std(X_train,0)+1e-5)
+        X_val=(X_val-np.mean(X_val,0))/(np.std(X_val,0)+1e-5)
+    if arg.binary == True:
+        X_train=binary_data(X_train)
+        X_val=binary_data(X_val)
     X_test=None
     label_test=None
     if arg.test_data!=None:
@@ -52,7 +65,10 @@ def get_data(arg):
             X_test=X_test.toarray()
             ind=np.where(label_test==0)
             label_test[ind]=-1
-            X_test=(X_test-np.mean(X_test,0))/(np.std(X_test,0)+1e-5)
+            if arg.norm==True:
+                X_test=(X_test-np.mean(X_test,0))/(np.std(X_test,0)+1e-5)
+            if arg.binary == True:
+                X_test=binary_data(X_test)
     data={"X_train":X_train,"label_train":label_train,"X_val":X_val,"label_val":label_val,"X_test": X_test,"label_test":label_test}
     data=Namespace(**data)
     return data
