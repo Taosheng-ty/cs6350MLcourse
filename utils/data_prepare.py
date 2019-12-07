f6fc5184c4a39e9c893506a2c0023b11e4d71c34
Taosheng-ty <taoyang@cs.utah.edu>
2019-12-07 00:37:11 -0700

    add split data function in order to partition the data to several slots

--- utils/data_prepare.py
+++ utils/data_prepare.py
@@ -41,6 +41,19 @@ def binary_data(data):
     data=data*0
     data[pos_ind]=1
     return data
+def split_data(X,param):
+    n_interve=param.n_interve+1
+    ra=np.linspace(-1,1,n_interve)
+    ra[0]=np.NINF
+    ra[-1]=-np.NINF
+    output=np.copy(X)
+    for i in range(n_interve-1):
+#         print(ra)
+        ind=(X>ra[i])*(X<ra[i+1])
+#         print(ind)
+        output[ind]=i 
+    
+    return output
 def get_data(arg):
     X_val, label_val, num_features=read_libsvm(arg.val_data)
     X_train, label_train, num_features=read_libsvm(arg.train_data)
@@ -50,10 +63,14 @@ def get_data(arg):
     label_val[ind]=-1
     ind=np.where(label_train==0)
     label_train[ind]=-1
+    
     if arg.norm==True:
         
         X_train=(X_train-np.mean(X_train,0))/(np.std(X_train,0)+1e-5)
         X_val=(X_val-np.mean(X_val,0))/(np.std(X_val,0)+1e-5)
+        if hasattr(arg,"n_interve"):
+             X_train=split_data(X_train,arg)
+             X_val=split_data( X_val,arg)
     if arg.binary == True:
         X_train=binary_data(X_train)
         X_val=binary_data(X_val)
@@ -67,6 +84,8 @@ def get_data(arg):
             label_test[ind]=-1
             if arg.norm==True:
                 X_test=(X_test-np.mean(X_test,0))/(np.std(X_test,0)+1e-5)
+                if hasattr(arg,"n_interve"):
+                     X_test=split_data(X_test,arg)
             if arg.binary == True:
                 X_test=binary_data(X_test)
     data={"X_train":X_train,"label_train":label_train,"X_val":X_val,"label_val":label_val,"X_test": X_test,"label_test":label_test}
